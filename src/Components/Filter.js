0be5a97423d9b9951634453bf0c9e8cad115d010
vartika06 <vartika.1998@gmail.com>
2020-03-18 17:24:02 +0530

    thanks page modified

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,35 +1,35 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import Calendar from "./Calendar";
+import Calendar from './Calendar';
 
 class Filter extends React.Component {
-  state = {
-    show: false
-  };
+	state = {
+		show: false
+	};
 
-  showModal = () => this.setState({ show: true });
-  hideModal = () => this.setState({ show: false });
-  render() {
-    return (
-      <div>
-        <button className="filterBtn" onClick={this.showModal}>
-          Select Filter
-        </button>
-        <Calendar
-          applyDates={this.props.applyDates}
-          addRange={this.props.addRange}
-          show={this.state.show}
-          hideModal={this.hideModal}
-        />
-      </div>
-    );
-  }
+	showModal = () => this.setState({ show: true });
+	hideModal = () => this.setState({ show: false });
+	render() {
+		return (
+			<div>
+				<button className='filterBtn' onClick={this.showModal}>
+					Select Date
+				</button>
+				<Calendar
+					applyDates={this.props.applyDates}
+					addRange={this.props.addRange}
+					show={this.state.show}
+					hideModal={this.hideModal}
+				/>
+			</div>
+		);
+	}
 }
 
 Filter.propTypes = {
-  addRange: PropTypes.func.isRequired,
-  applyDates: PropTypes.func.isRequired
+	addRange: PropTypes.func.isRequired,
+	applyDates: PropTypes.func.isRequired
 };
 
 export default Filter;
--- src/Components/Thanks.js
+++ src/Components/Thanks.js
@@ -1,58 +1,66 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Thanks extends React.Component {
-  componentDidMount() {
-    window.location.href = `http://py.lyzer.io/getCSV?companyKey=${localStorage.getItem(
-      "company"
-    )}&fromDate=${localStorage.getItem("unix1")}&toDate=${localStorage.getItem(
-      "unix2"
-    )}`;
-    window.location.href = `http://py.lyzer.io/getCSVSKUWise?companyKey=${localStorage.getItem(
-      "company"
-    )}&fromDate=${localStorage.getItem("unix1")}&toDate=${localStorage.getItem(
-      "unix2"
-    )}`;
-  }
-  render() {
-    // const day1 = parseInt(this.props.info.range[0].d);
-    // const day2 = parseInt(this.props.info.range[1].d);
+	componentDidMount() {
+		// window.location.href = `http://py.lyzer.io/getCSV?companyKey=${localStorage.getItem(
+		//   "company"
+		// )}&fromDate=${localStorage.getItem("unix1")}&toDate=${localStorage.getItem(
+		//   "unix2"
+		// )}`;
+		// window.location.href = `http://py.lyzer.io/getCSVSKUWise?companyKey=${localStorage.getItem(
+		//   "company"
+		// )}&fromDate=${localStorage.getItem("unix1")}&toDate=${localStorage.getItem(
+		//   "unix2"
+		// )}`;
+	}
+	render() {
+		// const day1 = parseInt(this.props.info.range[0].d);
+		// const day2 = parseInt(this.props.info.range[1].d);
 
-    // const month1 = parseInt(this.props.info.range[0].m);
-    // const month2 = parseInt(this.props.info.range[1].m);
+		// const month1 = parseInt(this.props.info.range[0].m);
+		// const month2 = parseInt(this.props.info.range[1].m);
 
-    // const year1 = parseInt(this.props.info.range[0].y);
-    // const year2 = parseInt(this.props.info.range[1].y);
-    console.log(this.props.info);
-    const link1 = `http://py.lyzer.io/getCSV?companyKey=${localStorage.getItem(
-      "company"
-    )}&fromDate=${localStorage.getItem("unix1")}&toDate=${localStorage.getItem(
-      "unix2"
-    )}`;
-    const link2 = `http://py.lyzer.io/getCSVSKUWise?companyKey=${localStorage.getItem(
-      "company"
-    )}&fromDate=${localStorage.getItem("unix1")}&toDate=${localStorage.getItem(
-      "unix2"
-    )}`;
-    return (
-      <div>
-        <h1 className="thanksHeading">
-          Thanks {localStorage.getItem("company")}
-        </h1>
-        <p className="thanksContent">
-          {" "}
-          Your csv will be downloaded in a minute.
-        </p>
-        <p>{link1}</p>
-        <p>{link2}</p>
-      </div>
-    );
-  }
+		// const year1 = parseInt(this.props.info.range[0].y);
+		// const year2 = parseInt(this.props.info.range[1].y);
+		console.log(this.props.info);
+		const link1 = `http://py.lyzer.io/getCSV?companyKey=${localStorage.getItem(
+			'company'
+		)}&fromDate=${localStorage.getItem('unix1')}&toDate=${localStorage.getItem('unix2')}`;
+		const link2 = `http://py.lyzer.io/getCSVSKUWise?companyKey=${localStorage.getItem(
+			'company'
+		)}&fromDate=${localStorage.getItem('unix1')}&toDate=${localStorage.getItem('unix2')}`;
+		return (
+			<div className='container-fluid'>
+				<div className='thanks-div'>
+					<div className='row'>
+						<h1 className='thanksHeading'>Thanks {localStorage.getItem('company')}</h1>
+					</div>
+					<div className='row'>
+						<p className='thanksContent'> Your csv will be downloaded in a minute.</p>
+					</div>
+					<div className='row'>
+						<button className='btn csv-btns'>
+							<a href={link1} target='_blank' style={{ textDecoration: 'none', color: '#fff' }}>
+								Get CSV
+							</a>
+						</button>
+
+						<button className='btn csv-btns'>
+							<a href={link2} target='_blank' style={{ textDecoration: 'none', color: '#fff' }}>
+								Get CSV (SKU)
+							</a>
+						</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 Thanks.propTypes = {
-  info: PropTypes.object.isRequired
+	info: PropTypes.object.isRequired
 };
 
 export default Thanks;
